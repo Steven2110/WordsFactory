4a288de4ed0537d46556524f769a2aae155e47c0
Steven2110 <steven.wijaya2001@gmail.com>
2022-04-15 13:47:20 +0700

    Initial Commit

---
+++ WordsFactory/WordsFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsFactoryApp.swift
+//  WordsFactory
+//
+//  Created by Steven Wijaya on 15.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
